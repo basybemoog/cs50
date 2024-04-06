84b69bd56fecec6dc2b255cf81acfcea433be6e9
fastrapier <basybemoog@gmail.com>
2024-04-07 01:53:35 +0500

    Solved Substitution

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"CS50/problems/week_2/Substitution"
+	"fmt"
+)
+
+func main() {
+	chippedText := Substitution.Substitution("YTNSHKVEFXRBAUQZCLWDMIPGJO", "Hello!")
+	fmt.Printf(chippedText)
+}
