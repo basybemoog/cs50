365fc0956deab342aabfe52ae4c6899bade4ee18
daniilstudenikin <fastrapier1@yandex.ru>
2024-04-06 13:26:07 +0300

    easify test

--- tests/week_2/scrabble/scrabble_test.go
+++ tests/week_2/scrabble/scrabble_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+const TIE = "Tie!"
+const Player1Win = "Player 1 wins!"
+const Player2Win = "Player 2 wins!"
+
 func TestScrabble(t *testing.T) {
 	tests := []struct {
 		name       string
@@ -12,15 +16,15 @@ func TestScrabble(t *testing.T) {
 		wordSecond string
 		want       string
 	}{
-		{"handles letter cases correctly", "LETTERCASE", "lettercase", "Tie!"},
-		{"handles punctuation correctly", "Punctuation!?!?", "punctuation", "Tie!"},
-		{"correctly identifies 'Question?' and 'Question!' as a tie", "Question?", "Question!", "Tie!"},
-		{"correctly identifies 'drawing' and 'illustration' as a tie", "drawing", "illustration", "Tie!"},
-		{"correctly identifies 'hai!' as winner over 'Oh,'", "Oh,", "hai!", "Player 2 wins!"},
-		{"correctly identifies 'COMPUTER' as winner over 'science'", "COMPUTER", "science", "Player 1 wins!"},
-		{"correctly identifies 'Scrabble' as winner over 'wiNNeR'", "Scrabble", "wiNNeR", "Player 1 wins!"},
-		{"correctly identifies 'pig' as winner over 'dog'", "pig", "dog", "Player 1 wins!"},
-		{"correctly identifies 'Skating!' as winner over 'figure?'", "figure?", "Skating!", "Player 2 wins!"},
+		{"handles letter cases correctly", "LETTERCASE", "lettercase", TIE},
+		{"handles punctuation correctly", "Punctuation!?!?", "punctuation", TIE},
+		{"correctly identifies 'Question?' and 'Question!' as a tie", "Question?", "Question!", TIE},
+		{"correctly identifies 'drawing' and 'illustration' as a tie", "drawing", "illustration", TIE},
+		{"correctly identifies 'hai!' as winner over 'Oh,'", "Oh,", "hai!", Player2Win},
+		{"correctly identifies 'COMPUTER' as winner over 'science'", "COMPUTER", "science", Player1Win},
+		{"correctly identifies 'Scrabble' as winner over 'wiNNeR'", "Scrabble", "wiNNeR", Player1Win},
+		{"correctly identifies 'pig' as winner over 'dog'", "pig", "dog", Player1Win},
+		{"correctly identifies 'Skating!' as winner over 'figure?'", "figure?", "Skating!", Player2Win},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
