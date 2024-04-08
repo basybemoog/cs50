f2d8601f676fafebdf9105f4f903912893e13ed4
fastrapier <basybemoog@gmail.com>
2024-04-08 13:47:24 +0500

    hide main go

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"CS50/problems/week_1/credit/problems/week_2/readability"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf(readability.Readability("One fish. two fish. three fish. white fish."))
+}
