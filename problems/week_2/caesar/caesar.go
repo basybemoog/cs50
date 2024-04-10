5b7fcc0a75b07bce054c5e066b055e17089ce9c8
basybemoog <basybemoog@gmail.com>
2024-04-10 22:58:46 +0500

    create sonarcloud

---
+++ problems/week_2/caesar/caesar.go
@@ -0,0 +1,31 @@
+package caesar
+
+import "strings"
+
+func Cipher(r rune, key int) rune {
+	char := int(r) + key
+	if r >= 'a' && r <= 'z' {
+
+		if char > 'z' {
+			return rune(char - 26)
+		} else if char < 'a' {
+			return rune(char + 26)
+		}
+	} else if r >= 'A' && r <= 'Z' {
+
+		if char > 'Z' {
+			return rune(char - 26)
+		} else if char < 'A' {
+			return rune(char + 26)
+		}
+	} else {
+		return r
+	}
+	return rune(char)
+}
+func Caesar(plaintText string, key int) string {
+	chipperText := strings.Map(func(r rune) rune {
+		return Cipher(r, key)
+	}, plaintText)
+	return chipperText
+}
