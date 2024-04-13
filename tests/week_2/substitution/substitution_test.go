dabe9a53ba9a12170f99b7e4ddc74974e1df8c62
daniilstudenikin <fastrapier1@yandex.ru>
2024-04-13 11:51:50 +0300

    fix substitution_test.go sonar issues

--- tests/week_2/substitution/substitution_test.go
+++ tests/week_2/substitution/substitution_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+const CS50Answer = "Cbah ah KH50"
+const CS50Question = "This is CS50"
+const DUPLICATEANSWER = "duplicate characters"
+
 func TestSubstitution(t *testing.T) {
 	type args struct {
 		key  string
@@ -19,17 +23,17 @@ func TestSubstitution(t *testing.T) {
 		{"encrypts \"a\" as \"z\" using ZYXWVUTSRQPONMLKJIHGFEDCBA as key", args{"ZYXWVUTSRQPONMLKJIHGFEDCBA", "a"}, "ciphertext: z"},
 		{"encrypts \"ABC\" as \"NJQ\" using NJQSUYBRXMOPFTHZVAWCGILKED as key", args{"NJQSUYBRXMOPFTHZVAWCGILKED", "ABC"}, "ciphertext: NJQ"},
 		{"encrypts \"XyZ\" as \"KeD\" using NJQSUYBRXMOPFTHZVAWCGILKED as key", args{"NJQSUYBRXMOPFTHZVAWCGILKED", "XyZ"}, "ciphertext: KeD"},
-		{"encrypts \"This is CS50\" as \"Cbah ah KH50\" using YUKFRNLBAVMWZTEOGXHCIPJSQD as key", args{"YUKFRNLBAVMWZTEOGXHCIPJSQD", "This is CS50"}, "ciphertext: Cbah ah KH50"},
-		{"encrypts \"This is CS50\" as \"Cbah ah KH50\" using yukfrnlbavmwzteogxhcipjsqd as key", args{"yukfrnlbavmwzteogxhcipjsqd", "This is CS50"}, "ciphertext: Cbah ah KH50"},
-		{"encrypts \"This is CS50\" as \"Cbah ah KH50\" using YUKFRNLBAVMWZteogxhcipjsqd as key", args{"YUKFRNLBAVMWZteogxhcipjsqd", "This is CS50"}, "ciphertext: Cbah ah KH50"},
+		{"encrypts \"This is CS50\" as \"Cbah ah KH50\" using YUKFRNLBAVMWZTEOGXHCIPJSQD as key", args{"YUKFRNLBAVMWZTEOGXHCIPJSQD", CS50Question}, "ciphertext: " + CS50Answer},
+		{"encrypts \"This is CS50\" as \"Cbah ah KH50\" using yukfrnlbavmwzteogxhcipjsqd as key", args{"yukfrnlbavmwzteogxhcipjsqd", CS50Question}, "ciphertext: " + CS50Answer},
+		{"encrypts \"This is CS50\" as \"Cbah ah KH50\" using YUKFRNLBAVMWZteogxhcipjsqd as key", args{"YUKFRNLBAVMWZteogxhcipjsqd", CS50Question}, "ciphertext: " + CS50Answer},
 		{"encrypts all alphabetic characters using DWUSXNPQKEGCZFJBTLYROHIAVM as key", args{"DWUSXNPQKEGCZFJBTLYROHIAVM", "The quick brown fox jumps over the lazy dog"}, "ciphertext: Rqx tokug wljif nja eozby jhxl rqx cdmv sjp"},
 		{"does not encrypt non-alphabetical characters using DWUSXNPQKEGCZFJBTLYROHIAVM as key", args{"DWUSXNPQKEGCZFJBTLYROHIAVM", "Shh... Don't tell!"}, "ciphertext: Yqq... Sjf'r rxcc!"},
 		{"handles lack of key", args{"", ""}, "Key must contain 26 characters"},
 		{"handles invalid key length", args{"QTXDGMKIPV", ""}, "Key must contain 26 characters"},
 		{"handles invalid characters in key", args{"ZWGKPMJ^YISHFEXQON[DLUACVT", ""}, "Invalid characters in key"},
-		{"handles duplicate characters in uppercase key", args{"FAZRDTMGQEJPWAXUSKVIYCLONH", ""}, "duplicate characters"},
-		{"handles duplicate characters in lowercase key", args{"fazrdtmgqejpwaxuskviyclonh", ""}, "duplicate characters"},
-		{"handles multiple duplicate characters in key", args{"MMCcEFGHIJKLMNOPqRqTUVWXeZ", ""}, "duplicate characters"},
+		{"handles duplicate characters in uppercase key", args{"FAZRDTMGQEJPWAXUSKVIYCLONH", ""}, DUPLICATEANSWER},
+		{"handles duplicate characters in lowercase key", args{"fazrdtmgqejpwaxuskviyclonh", ""}, DUPLICATEANSWER},
+		{"handles multiple duplicate characters in key", args{"MMCcEFGHIJKLMNOPqRqTUVWXeZ", ""}, DUPLICATEANSWER},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
