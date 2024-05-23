b7ab0bf96640af28086d361adb0778a5202036f3
basybemoog <basybemoog@gmail.com>
2024-05-23 15:49:42 +0500

    cash styled

--- problems/week_6/cash_py/cash.py
+++ problems/week_6/cash_py/cash.py
@@ -1,21 +1,22 @@
 def main():
     value = get_float()
-    value = round(value*100)
+    value = round(value * 100)
     counter = 0
-    while value >=25:
+    while value >= 25:
         value -= 25
-        counter+=1
-    while value >=10:
+        counter += 1
+    while value >= 10:
         value -= 10
-        counter+=1
-    while value >=5:
+        counter += 1
+    while value >= 5:
         value -= 5
-        counter+=1
-    while value >=1:
-        value-=1
-        counter+=1
+        counter += 1
+    while value >= 1:
+        value -= 1
+        counter += 1
 
     print(counter)
+
 
 def get_float():
     while True:
@@ -25,5 +26,6 @@ def get_float():
                 return n
         except ValueError:
             print('Not an integer')
+
 
 main()
